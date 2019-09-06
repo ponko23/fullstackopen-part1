2ccc4a0281bb6d3e7d0b3ee984849b7322c9c0e0
ponko23 <kohe.mizu@gmail.com>
2019-09-06 17:22:06 +0900

    1.7 finished

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -4,25 +4,52 @@ import ReactDOM from 'react-dom'
 const Button = ({onClick, text}) =>
     <button onClick={onClick}>{text}</button>
 
-const Statistics = ({text, count}) =>
-    <p>{text} {count}</p>
+const Statistics = ({text}) =>
+    <p>{text}</p>
 
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
+  const addGood = () => {
+    setGood(good + 1)
+    setAll(all + 1)
+    setAverage((good + 1 - bad) / (all + 1))
+    setPositive((good + 1) / (all + 1) * 100)
+  }
+
+  const addNeutral = () => {
+    setNeutral(neutral + 1)
+    setAll(all + 1)
+    setAverage((good - bad) / (all + 1))
+    setPositive(good / (all + 1) * 100)
+  }
+
+  const addBad = () => {
+    setBad(bad + 1)
+    setAll(all + 1)
+    setAverage((good - bad - 1) / (all + 1))
+    setPositive(good / (all + 1) * 100)
+  }
 
   return (
     <div>
         <h1>give feedback</h1>
-        <Button onClick={() => setGood(good + 1)} text='good' />
-        <Button onClick={() => setNeutral(neutral + 1)} text='neutral' />
-        <Button onClick={() => setBad(bad + 1)} text='bad' />
+        <Button onClick={addGood} text='good' />
+        <Button onClick={addNeutral} text='neutral' />
+        <Button onClick={addBad} text='bad' />
         <h2>statistics</h2>
-        <Statistics text='good' count={good} />
-        <Statistics text='neutral' count={neutral} />
-        <Statistics text='bad' count={bad} />
+        <Statistics text={`good ${good}`} />
+        <Statistics text={`neutral ${neutral}`} />
+        <Statistics text={`bad ${bad}`} />
+        <Statistics text={`all ${all}`} />
+        <Statistics text={`average ${average}`} />
+        <Statistics text={`positive ${positive}%`} />
     </div>
   )
 }
