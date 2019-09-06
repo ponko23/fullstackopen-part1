ff1139aa01b939f5c8d2228f5197a5036dd0b5eb
ponko23 <kohe.mizu@gmail.com>
2019-09-06 21:59:20 +0900

    1.14 finished

--- anecdotes/src/index.js
+++ anecdotes/src/index.js
@@ -1,9 +1,15 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = ({ text }) => <h1>{text}</h1>
+const Button = ({clickHandler, text}) => <button onClick={clickHandler}>{text}</button>
+const Anecdote = ({text}) => <p>{text}</p>
+const Vote = ({vote}) => <p>has {vote} vote</p>
+
 const App = ({ anecdotes }) => {
   const [selected, setSelected] = useState(0)
-  const [points, setPoints] =useState([0, 0, 0, 0 ,0 ,0])
+  const [points, setPoints] = useState([0, 0, 0, 0 ,0 ,0])
+  const [maxIndex, setMaxIndex] = useState(0)
 
   const setNextAnecdote = () => {
     const index = Math.floor(Math.random() * anecdotes.length)
@@ -15,14 +21,22 @@ const App = ({ anecdotes }) => {
       copy[selected] += 1
       setPoints(copy)
       console.log(copy)
+
+      let max = Math.max.apply(null, copy)
+      setMaxIndex(copy.indexOf(max))
+      
   }
   
   return (
     <div>
-      <p>{anecdotes[selected]}</p>
-      <p>has {points[selected]} vote</p>
-      <button onClick={voteSelectedAnecdote}>vote</button>
-      <button onClick={setNextAnecdote}>next anecdote</button>
+      <Header text='Anecdote of the day' />
+      <Anecdote text={anecdotes[selected]} />
+      <Vote vote={points[selected]} />
+      <Button clickHandler={voteSelectedAnecdote} text='vote' />
+      <Button clickHandler={setNextAnecdote} text='next anecdote' />
+      <Header text='Anecdote with most votes' />
+      <Anecdote text={anecdotes[maxIndex]} />
+      <Vote vote={points[maxIndex]} />
     </div>
   )
 }
